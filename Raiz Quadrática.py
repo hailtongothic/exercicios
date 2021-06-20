f975d7bd493f722d7de98f0d9e5b5bb8ceb86c13
hailtongothic <85834761+hailtongothic@users.noreply.github.com>
2021-06-20 18:07:15 -0300

    Raiz Quadrática (Resolvido)
    Aqui o professor ensinou como resolver, estou insatisfeito por eu mesmo não conseguir resolver!

--- Raiz Quadrática.py
+++ Raiz Quadrática.py
@@ -1,12 +1,18 @@
-a = int(input('Digite o primeiro número: '))
-b = int(input('Digite o segundo número: '))
-c = int(input('Digite o terceiro número: '))
+import math
 
-delta = (b ** 2) - 4 * a * c
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valro de c: '))
 
-if delta <0:
-    print('Não tem Raiz Real!')
+delta = b ** 2 - 4 * a * c
 if delta == 0:
-    print('Tem uma Raiz Real!')
-if delta > 0:
-    print('Tem duas Raizes Reais que são: {}, {} {}'.format(a, b, c))
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    print('A única raiz é:', raiz1)
+else:
+    if delta < 0:
+        print('Esta equação não possui raízes reais')
+    else:
+        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+        print('A primeira raiz é: ', raiz1)
+        print('A segunda raiz é: ', raiz2)
