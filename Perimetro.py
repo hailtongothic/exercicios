939f25ac15d425e61591822272d3008622aec098
hailtongothic <85834761+hailtongothic@users.noreply.github.com>
2021-06-20 10:55:51 -0300

    Calcula o Perímetro e a Área de um quadrado
    Como falei, o chato é ter que correr atrás das fórmulas matemáticas e praticamente "converter" para a linguagem de programação! Sou péssimo em matemática.

---
+++ Perimetro.py
@@ -0,0 +1,4 @@
+perimetro = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+calculoperimetro = 4 * (perimetro)
+area = perimetro ** 2
+print('perimetro:{} - área:{}'.format(calculoperimetro, area))
